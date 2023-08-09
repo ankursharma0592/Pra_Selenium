ef2319fb25986e0fe40e9b5da0e8e26d3d2ddeb6
Ankur <ankursharma.0591@gmail.com>
2023-08-09 17:32:47 +0530

    Learning Selenium 3rd Aug

---
+++ First_Program.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://app.vwo.com/#/login")
---
+++ tests/test_01_vwologin.py
@@ -0,0 +1,27 @@
+import time
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_vwologin():
+    logger = logging.getLogger(__name__)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    driver.get("https://app.vwo.com/#/login")
+
+    email_address = driver.find_element(By.ID, "login-username")
+    email_address.send_keys("ankursharma.0592@yahoo.com")
+
+    password = driver.find_element(By.ID, "login-password")
+    password.send_keys("As@9669302381")
+
+    button = driver.find_element(By.ID, "js-login-btn")
+    button.click()
+
+    time.sleep(10)
+    logger.info("title is"+driver.title)
+    assert "Dashboard" in driver.title
